8703782a476395151c7abea2b313abc5b8f689df
SPToss <sebastian.lukasz.pawlikowski@gmail.com>
2019-08-18 13:39:38 +0200

    Test Commit.

--- DebugConsole/Program.cs
+++ DebugConsole/Program.cs
@@ -6,7 +6,7 @@ namespace DebugConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! + Some Test");
         }
     }
 }
